48a02495b922b34908a41f1445c33008a06caf47
Cole Craft <colecraft.cc@gmail.com>
2021-05-21 15:48:57 -0400

    Completed

--- code.js
+++ code.js
@@ -2,3 +2,20 @@
 // and select "Open with Live Server"
 
 // Your Code Here.
+
+let submitButton = document.getElementById('formButton');
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    let nameInput = document.getElementById('name_input');
+    let emailInput = document.getElementById('email_input');
+    let messageArea = document.getElementById('message_area');
+
+    let nameInputValue = nameInput.value;
+    let emailInputValue = emailInput.value;
+    let messageAreaValue = messageArea.value;
+
+    alert(`You have a message from: ${nameInputValue} \nWhich says: ${messageAreaValue}. \nMessage them back at: ${emailInputValue}.`)
+})
+
